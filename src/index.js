5c246ddb290c9b2aed88b6067808e2e3dd8c20ed
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-01-11 21:27:04 +0700

    done first action

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import store from './store';
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import './index.css';
 
 ReactDOM.render(
-  <App stories={ store.getState().storyState } onArchive={ () => {} } />,
+  <App 
+    stories={ store.getState().storyState } 
+    onArchive={ id => store.dispatch({ type: STORY_ARCHIVE, id }) } 
+  />,
   document.getElementById('root')
 );
--- src/reducers/archive.js
+++ src/reducers/archive.js
@@ -1,7 +1,16 @@
+import { STORY_ARCHIVE } from '../constants/actionTypes';
+
 const INITIAL_STATE = [];
+
+const applyArchiveStory = (state, action) => {
+  return [ ...state, action.id ];
+};
 
 const archiveReducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
+    case STORY_ARCHIVE: {
+      return applyArchiveStory;
+    }
     default: return state;
   }
 };
